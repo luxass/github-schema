ec2c66982968ac0af2d9c0ae117e4a1dd0d77d7e
Lucas Nørgård <lucasnrgaard@gmail.com>
2023-12-01 05:46:09 +0100

    fix: remove bun from download script

--- scripts/download.ts
+++ scripts/download.ts
@@ -1,26 +1,33 @@
-#!/usr/bin/env bun
 import process from "node:process";
+import {
+  writeFile,
+} from "node:fs/promises";
 
 async function run() {
-  if (!Bun.env.GITHUB_TOKEN) {
-    console.log("GITHUB_TOKEN not set");
+  if (!process.env.GITHUB_TOKEN) {
+    console.error("GITHUB_TOKEN not set");
     process.exit(1);
   }
 
   const res = await fetch("https://api.github.com/graphql", {
     headers: {
-      Authorization: `bearer ${Bun.env.GITHUB_TOKEN}`,
+      Authorization: `bearer ${process.env.GITHUB_TOKEN}`,
       Accept: "application/vnd.github.v4.idl",
     },
   });
 
   if (!res.ok) {
-    console.log("Failed to fetch schema");
+    console.error("Failed to fetch schema");
     process.exit(1);
   }
 
   const schema = await res.json();
-  await Bun.write("github-schema.graphql", schema.data);
+  if (!schema || typeof schema !== "object" || !("data" in schema) || typeof schema.data !== "string") {
+    console.error("Failed to parse schema");
+    process.exit(1);
+  }
+
+  await writeFile("github-schema.graphql", schema.data);
 }
 
 run().catch((err) => {
