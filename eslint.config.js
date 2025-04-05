43926b8a4507eae3438177b26267626571c32c29
Lucas Nørgård <lucasnrgaard@gmail.com>
2025-04-05 10:34:29 +0100

    chore: fix eslint

--- eslint.config.js
+++ eslint.config.js
@@ -2,8 +2,10 @@
 import luxass from "@luxass/eslint-config";
 
 export default luxass({
-  formatters: {
-    markdown: true,
-    graphql: false,
-  },
+  type: "lib",
+}, {
+  ignores: [
+    "github-schema.graphql",
+    "./src/github-schema.ts",
+  ],
 });
